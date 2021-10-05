c1342325d687448c6b80e0ca020193c0d85a5e5e
Your Name <you@example.com>
2021-10-05 07:42:26 -0300

    Entrada de dados com o teclado

--- exercicioaluno/src/testes/AlunoTeste.java
+++ exercicioaluno/src/testes/AlunoTeste.java
@@ -1,21 +1,37 @@
 package testes;
 
+import javax.swing.JOptionPane;
+
 import classes.Aluno;
 
 public class AlunoTeste {
 	public static void main(String[] args) {
+		
+		String nome = JOptionPane.showInputDialog("Qual é o nome do aluno?");
+		String idade = JOptionPane.showInputDialog("Qual a idade");
+		String data = JOptionPane.showInputDialog("Qual a data de nascimento?");
+		String cpf = JOptionPane.showInputDialog("Qual o CPF?");
+		String mae = JOptionPane.showInputDialog("Qual o nome da mãe?");
+		String pai = JOptionPane.showInputDialog("Qual o nome da pai?");
+		String instituicao = JOptionPane.showInputDialog("Qual o nome da instituição de ensino?");
+		String nota1 = JOptionPane.showInputDialog("Qual a nota 1?");
+		String nota2 = JOptionPane.showInputDialog("Qual a nota 2?");
+		String nota3 = JOptionPane.showInputDialog("Qual a nota 3?");
+		String nota4 = JOptionPane.showInputDialog("Qual a nota 4?");
+		
 		Aluno aluno1 = new Aluno();
-		aluno1.setNome("Simon Assagra");
-		aluno1.setIdade(19);
-		aluno1.setDataNascimento("01/10/2002");
-		aluno1.setCpf("999.999.999-99");
-		aluno1.setNomeMae("Márcia");
-		aluno1.setNomePai("Higor");
-		aluno1.setNomeInstituicao("Instituto Infnet");
-		aluno1.setNota1(7);
-		aluno1.setNota2(6.5);
-		aluno1.setNota3(6);
-		aluno1.setNota4(7.9);
+		
+		aluno1.setNome(nome);
+		aluno1.setIdade(Integer.valueOf(idade));
+		aluno1.setDataNascimento(data);
+		aluno1.setCpf(cpf);
+		aluno1.setNomeMae(mae);
+		aluno1.setNomePai(pai);
+		aluno1.setNomeInstituicao(instituicao);
+		aluno1.setNota1(Double.parseDouble(nota1));
+		aluno1.setNota2(Double.parseDouble(nota2));
+		aluno1.setNota3(Double.parseDouble(nota3));
+		aluno1.setNota4(Double.parseDouble(nota4));
 		
 		System.out.println("Nome do aluno: " + aluno1.getNome());
 		System.out.println("Idade: " + aluno1.getIdade());
