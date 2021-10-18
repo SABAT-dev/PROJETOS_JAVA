c9bb3bb769dee29e658a4525c5ba352db6c22f2e
Your Name <you@example.com>
2021-10-18 18:22:23 -0300

    Criando e Percorrendo a lista de alunos

--- exercicioaluno/src/testes/AlunoTeste.java
+++ exercicioaluno/src/testes/AlunoTeste.java
@@ -1,5 +1,8 @@
 package testes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.JOptionPane;
 import classes.Aluno;
 import classes.Disciplina;
@@ -7,57 +10,67 @@ import classes.Disciplina;
 public class AlunoTeste {
 	public static void main(String[] args) {
 		
-		String nome = JOptionPane.showInputDialog("Qual é o nome do aluno?");
-		/*String idade = JOptionPane.showInputDialog("Idade?");
-		String data = JOptionPane.showInputDialog("Data de nascimento?");
-		String cpf = JOptionPane.showInputDialog("CPF?");
-		String mae = JOptionPane.showInputDialog("Nome da mãe?");
-		String pai = JOptionPane.showInputDialog("Nome da pai?");
-		String instituicao = JOptionPane.showInputDialog("Nome da instituição de ensino?");*/
+		List<Aluno> alunos = new ArrayList<Aluno>();
 		
-		Aluno aluno1 = new Aluno();
+		for (int numeroAlunos = 1; numeroAlunos <= 2; numeroAlunos++) {
 		
-		aluno1.setNome(nome);
-		/*aluno1.setIdade(Integer.valueOf(idade));
-		aluno1.setDataNascimento(data);
-		aluno1.setCpf(cpf);
-		aluno1.setNomeMae(mae);
-		aluno1.setNomePai(pai);
-		aluno1.setNomeInstituicao(instituicao);*/
-		
-		for (int posicao = 1; posicao <= 4; posicao++) {
+			String nome = JOptionPane.showInputDialog("Qual é o nome do aluno " + numeroAlunos + "?");
+			/*String idade = JOptionPane.showInputDialog("Idade?");
+			String data = JOptionPane.showInputDialog("Data de nascimento?");
+			String cpf = JOptionPane.showInputDialog("CPF?");
+			String mae = JOptionPane.showInputDialog("Nome da mãe?");
+			String pai = JOptionPane.showInputDialog("Nome da pai?");
+			String instituicao = JOptionPane.showInputDialog("Nome da instituição de ensino?");*/
 			
-			Disciplina disciplina = new Disciplina();
+			Aluno aluno1 = new Aluno();
 			
-			String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + posicao + "?");
-			String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + posicao + "?");
-			disciplina.setDisciplina(nomeDisciplina);
-			disciplina.setNota(Double.valueOf(notaDisciplina));
-			aluno1.getDisciplinas().add(disciplina);
-		}
-		
-		int decisao = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
-		
-		if(decisao == 0) {
-			
-			int continuarRemovendo = 0;
-			int posicao = 1;
-			while (continuarRemovendo == 0) {
-				String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina quer remover 1,2,3 ou 4?");
-				aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() - posicao);
-				posicao++;
-				continuarRemovendo = JOptionPane.showConfirmDialog(null, "Deseja continuar removendo?");
+			aluno1.setNome(nome);
+			/*aluno1.setIdade(Integer.valueOf(idade));
+			aluno1.setDataNascimento(data);
+			aluno1.setCpf(cpf);
+			aluno1.setNomeMae(mae);
+			aluno1.setNomePai(pai);
+			aluno1.setNomeInstituicao(instituicao);*/
+					
+			for (int posicaoDisciplinas = 1; posicaoDisciplinas <= 4; posicaoDisciplinas++) {
+				
+				Disciplina disciplina = new Disciplina();
+				
+				String nomeDisciplina = JOptionPane.showInputDialog("Nome da disciplina " + posicaoDisciplinas + "?");
+				String notaDisciplina = JOptionPane.showInputDialog("Nota da disciplina " + posicaoDisciplinas + "?");
+				disciplina.setDisciplina(nomeDisciplina);
+				disciplina.setNota(Double.valueOf(notaDisciplina));
+				aluno1.getDisciplinas().add(disciplina);
 			}
+			
+			int decisao = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+			
+			if(decisao == 0) {
+				
+				int continuarRemovendo = 0;
+				int posicao = 1;
+				while (continuarRemovendo == 0) {
+					String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina quer remover 1,2,3 ou 4?");
+					aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() - posicao);
+					posicao++;
+					continuarRemovendo = JOptionPane.showConfirmDialog(null, "Deseja continuar removendo?");
+				}
+			}
+			
+			alunos.add(aluno1);
 		}
 		
-		System.out.println("Nome do aluno: " + aluno1.getNome());
-		System.out.println("Idade: " + aluno1.getIdade());
-		System.out.println("Data de Nascimento: " + aluno1.getDataNascimento());
-		System.out.println("CPF: " + aluno1.getCpf());
-		System.out.println("Nome da mãe: " + aluno1.getNomeMae());
-		System.out.println("Nome do pai: " + aluno1.getNomePai());
-		System.out.println("Nome da Instituição: " + aluno1.getNomeInstituicao());
-		System.out.printf("Média final: %.1f\n", aluno1.getMediaFinal());
-		System.out.println("Classificação final: " + (aluno1.getClassificacaoFinal()));
+		for (Aluno aluno : alunos) {
+			System.out.println("Nome do aluno: " + aluno.getNome());
+			System.out.println("Idade: " + aluno.getIdade());
+			System.out.println("Data de Nascimento: " + aluno.getDataNascimento());
+			System.out.println("CPF: " + aluno.getCpf());
+			System.out.println("Nome da mãe: " + aluno.getNomeMae());
+			System.out.println("Nome do pai: " + aluno.getNomePai());
+			System.out.println("Nome da Instituição: " + aluno.getNomeInstituicao());
+			System.out.printf("Média final: %.1f\n", aluno.getMediaFinal());
+			System.out.println("Classificação final: " + (aluno.getClassificacaoFinal()));	
+			System.out.println(".............................................................");
+		}
 	}
 }
