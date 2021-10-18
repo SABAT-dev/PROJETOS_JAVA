5b736c254c8e0feaea6f6c2370936da53ca00406
Your Name <you@example.com>
2021-10-18 16:28:29 -0300

    Remover disciplinas em repetição

--- exercicioaluno/src/testes/AlunoTeste.java
+++ exercicioaluno/src/testes/AlunoTeste.java
@@ -8,22 +8,22 @@ public class AlunoTeste {
 	public static void main(String[] args) {
 		
 		String nome = JOptionPane.showInputDialog("Qual é o nome do aluno?");
-		String idade = JOptionPane.showInputDialog("Idade?");
+		/*String idade = JOptionPane.showInputDialog("Idade?");
 		String data = JOptionPane.showInputDialog("Data de nascimento?");
 		String cpf = JOptionPane.showInputDialog("CPF?");
 		String mae = JOptionPane.showInputDialog("Nome da mãe?");
 		String pai = JOptionPane.showInputDialog("Nome da pai?");
-		String instituicao = JOptionPane.showInputDialog("Nome da instituição de ensino?");
+		String instituicao = JOptionPane.showInputDialog("Nome da instituição de ensino?");*/
 		
 		Aluno aluno1 = new Aluno();
 		
 		aluno1.setNome(nome);
-		aluno1.setIdade(Integer.valueOf(idade));
+		/*aluno1.setIdade(Integer.valueOf(idade));
 		aluno1.setDataNascimento(data);
 		aluno1.setCpf(cpf);
 		aluno1.setNomeMae(mae);
 		aluno1.setNomePai(pai);
-		aluno1.setNomeInstituicao(instituicao);
+		aluno1.setNomeInstituicao(instituicao);*/
 		
 		for (int posicao = 1; posicao <= 4; posicao++) {
 			
@@ -39,8 +39,15 @@ public class AlunoTeste {
 		int decisao = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
 		
 		if(decisao == 0) {
-			String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina quer remover 1,2,3 ou 4?");
-			aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() -1);
+			
+			int continuarRemovendo = 0;
+			int posicao = 1;
+			while (continuarRemovendo == 0) {
+				String removerDisciplina = JOptionPane.showInputDialog("Qual disciplina quer remover 1,2,3 ou 4?");
+				aluno1.getDisciplinas().remove(Integer.valueOf(removerDisciplina).intValue() - posicao);
+				posicao++;
+				continuarRemovendo = JOptionPane.showConfirmDialog(null, "Deseja continuar removendo?");
+			}
 		}
 		
 		System.out.println("Nome do aluno: " + aluno1.getNome());
