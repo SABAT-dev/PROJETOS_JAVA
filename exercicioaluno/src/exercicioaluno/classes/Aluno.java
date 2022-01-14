049f02e21b83c45c8c84e891a769b4c03714c034
Your Name <you@example.com>
2022-01-14 19:25:29 -0300

    Classes Abstratas

--- exercicioaluno/src/exercicioaluno/classes/Aluno.java
+++ exercicioaluno/src/exercicioaluno/classes/Aluno.java
@@ -122,4 +122,5 @@ public class Aluno extends Pessoa {
 			return AlunoConstante.REPROVADO;
 		}
 	}
+	
 }
--- exercicioaluno/src/exercicioaluno/classes/Pessoa.java
+++ exercicioaluno/src/exercicioaluno/classes/Pessoa.java
@@ -1,6 +1,6 @@
 package exercicioaluno.classes;
 
-public class Pessoa {
+public abstract class Pessoa {
 	protected String nome;
 	protected int idade;
 	protected String dataNascimento;
